d11826c2ab8ddd53ad9d5c9c5913e1afd07e7bb8
Laura Arranz Izquierdo <laura.arranz@vass.es>
2020-02-29 10:55:52 +0100

    kata-2

--- src/kata-2.js
+++ src/kata-2.js
@@ -1,3 +1,24 @@
 export function kata2 (obj, def, path) {
+    let result;
 
-}
+    path ?
+        result = getResultWithPath(obj, def, path) :
+        result = function(path2) { return kata2(obj, def, path2)};
+
+    return result;
+};
+
+function getResultWithPath (obj, def, path) {
+    const pathObject = path.split('.');
+    let result;
+
+    pathObject.forEach(level => {
+        obj && obj.hasOwnProperty(level) ?
+            obj = obj[level] :
+            obj = undefined;
+    });
+
+    !obj ? result = def : result = obj;
+
+    return result;
+};
